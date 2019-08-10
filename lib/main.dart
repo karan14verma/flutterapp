3655dc4b448f50f46bf38a71e45a4a99d60a4c81
Karan Verma <karan14verma@gmail.com>
2019-08-10 16:15:50 -0400

    stage 2 of demo

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
+      theme: ThemeData(          // Add the 3 lines from here... 
+        primaryColor: Colors.white,
+      ),  
       home: RandomWords(),
+      
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
@@ -38,12 +43,57 @@ class RandomWordsState extends State<RandomWords> {
       
   }
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
        pair.asPascalCase,
         style: _biggerFont,
      ),
+     trailing: Icon(   
+      alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null,
+    ),        
+    onTap: () {     
+      setState(() {
+        if (alreadySaved) {
+          _saved.remove(pair);
+        } else { 
+          _saved.add(pair); 
+        } 
+      });
+    }, 
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+      builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map(
+          (WordPair pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+          .toList();
+          return Scaffold(         // Add 6 lines from here...
+          appBar: AppBar(
+            title: Text('Saved Suggestions'),
+          ),
+          body: ListView(children: divided),
+        );     
+      },
+    ),                 
+  );
   }
 
   @override
@@ -51,7 +101,11 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
     appBar: AppBar(
       title: Text('Startup Name Generator'),
+      actions: <Widget>[ 
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],  
     ),
+    
     body: _buildSuggestions(),
   );
   }
